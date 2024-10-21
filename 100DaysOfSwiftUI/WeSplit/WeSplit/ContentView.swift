6542b1c385a7bb73e6621ea304de11d469cc809c
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2024-10-20 23:29:58 -0400

    add low tip color

--- 100DaysOfSwiftUI/WeSplit/WeSplit/ContentView.swift
+++ 100DaysOfSwiftUI/WeSplit/WeSplit/ContentView.swift
@@ -50,6 +50,7 @@ struct ContentView: View {
 
                 Section("Total amount") {
                     Text(grandTotal, format: .currency(code: localCurrency))
+                        .foregroundStyle(tipPercentage == 0 ? .red : .primary)
                 }
 
                 Section("Amount per person") {
