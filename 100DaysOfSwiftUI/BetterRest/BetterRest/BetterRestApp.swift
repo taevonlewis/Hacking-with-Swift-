5fe9e0561e0b81d81d9c5ec3572240b2d379d8c4
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2024-10-21 22:05:34 -0400

    add starter files

---
+++ 100DaysOfSwiftUI/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by TaeVon Lewis on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
