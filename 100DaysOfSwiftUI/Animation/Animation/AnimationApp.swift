b03bbdb5419694436336a9aa2258c93d3ca8e183
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2024-10-23 23:22:31 -0400

    completed project 6: Animation

---
+++ 100DaysOfSwiftUI/Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by TaeVon Lewis on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 100DaysOfSwiftUI/Animation/Animation/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  Animation
+//
+//  Created by TaeVon Lewis on 10/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isShowingRed = false
+    
+    var body: some View {
+        ZStack {
+            Button("Tap Me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
+    }
+}
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
+#Preview {
+    ContentView()
+}
