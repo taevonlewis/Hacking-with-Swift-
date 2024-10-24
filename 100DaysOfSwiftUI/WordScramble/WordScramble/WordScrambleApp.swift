9bcbc8b8429f24af4da89bde827452668d47f4f6
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2024-10-23 21:10:43 -0400

    completed project 5: WordScramble

---
+++ 100DaysOfSwiftUI/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by TaeVon Lewis on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
