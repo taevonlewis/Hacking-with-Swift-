1ca6b4cdb2ba6d9bed607e5561fb23e60cbe148b
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2024-10-26 00:10:57 -0400

    create project 8: Moonshot

---
+++ 100DaysOfSwiftUI/Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by TaeVon Lewis on 10/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 100DaysOfSwiftUI/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by TaeVon Lewis on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
