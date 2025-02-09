6ecc74dabfd1696fd37c59adc35570224947e7c7
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2025-02-08 22:06:10 -0500

    starter code

---
+++ 100DaysOfSwiftUI/Challenges/Habit Tracker/Habit Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Habit Tracker
+//
+//  Created by TaeVon Lewis on 2/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 100DaysOfSwiftUI/Challenges/Habit Tracker/Habit Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit Tracker
+//
+//  Created by TaeVon Lewis on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
